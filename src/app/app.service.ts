fce4da3ce5551f7b3870ff424a176cf8cb2abe8c
Liam <liamjohnrobertson@gmail.com>
2021-12-03 22:45:06 +0000

    streaming final video works

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,7 +9,8 @@ export class AppService {
 
   constructor(private http: HttpClient) {}
 
-  getVideo(videoUrl: string): Observable<any> {
-    return this.http.get(videoUrl, {responseType: 'blob'});
+  getVideo(): Observable<any> {
+    const headers = new HttpHeaders({'Content-Type': 'video/mp4', 'Accept-Ranges':'bytes'});
+    return this.http.get("http://localhost:4200/api/final-video.mp4", {'headers': headers, responseType: 'blob'});
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  myVideo: any;
 
-  constructor() { }
+  constructor(
+    private appService: AppService
+  ) { }
 
   ngOnInit(): void {
+    this.appService.getVideo().subscribe(mp4 => {
+      let dataType = mp4.type;
+      let binaryData = [];
+      binaryData.push(mp4);
+      let videoEl = <HTMLVideoElement>document.getElementById("myVideoEl")
+      videoEl.src = window.URL.createObjectURL(new Blob(binaryData, {type: dataType}));
+      // let downloadLink = document.createElement('video');
+      // downloadLink.src = window.URL.createObjectURL(new Blob(binaryData, {type: dataType}));
+      // console.log(downloadLink.src);
+      document.body.appendChild(videoEl);
+      // if (filename)
+      //     downloadLink.setAttribute('download', filename);
+      // document.body.appendChild(downloadLink);
+      // downloadLink.click();
+      
+    });
   }
-
 }
